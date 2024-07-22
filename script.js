fc98c1e49765496ca50176b7cee2725724f2e126
Aryan Rana <117369011+aryanrana19@users.noreply.github.com>
2024-07-23 01:32:44 +0530

    new pages

---
+++ script.js
@@ -0,0 +1,6 @@
+function startDuel(){
+  document.getElementById("start-card").classList.add("hide")
+  document.getElementById("navbar").classList.add("hide-navbar")
+  document.querySelector('.p1-area').classList.add('enlarged');
+  document.querySelector('.p2-area').classList.add('enlarged');
+}
